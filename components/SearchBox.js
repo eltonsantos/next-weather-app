1092d8d6e015343639eb2532d7b0e06f71751291
Elton Santos <eltonaxl@hotmail.com>
2022-01-13 22:07:03 -0300

    Trying component searchbox work

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import cities from '../lib/city.list.json';
 
 export default function SearchBox() {
+
+  const [query, setQuery] = useState("");
+
+  const onChange = (e) => {
+    const { value } = e.target;
+    setQuery(value);
+
+    let matchingCities = [];
+
+    if (value.length > 3) {
+      for (let city of cities) {
+        if (matchingCities.length >= 5) {
+          break;
+        }
+
+        const match = city.name.toLowerCase().startsWith(value.toLowerCase());
+
+        if (match) {
+          matchingCities.push(city);
+        }
+      }
+    }
+
+    console.log(matchingCities);
+  };
+
   return (
     <div className="search">
-      <input type="text" />
+      <input type="text" value={query} onChange={onChange} />
     </div>
   )
 }
